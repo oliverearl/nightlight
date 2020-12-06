58f797b3bc3f03fbda091277bfb1d68bc707357a
Oliver Earl <oliver@oliverearl.co.uk>
2020-12-06 20:37:47 +0000

    Authorisation library

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-$route['test'] = 'TwigTest';
+$route['test'] = 'twigtest';
 $route['404_override'] = '';
+$route['LOGIN_PAGE'] = 'examples/login';
 $route['translate_uri_dashes'] = FALSE;
