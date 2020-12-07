c514ccf7ca18fa37e9c02ed2e770223b00697772
Oliver Earl <oliver@oliverearl.co.uk>
2020-12-07 00:19:52 +0000

    Controller for the homepage

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'Home_Controller';
 $route['test'] = 'twigtest';
 $route['404_override'] = '';
 $route['LOGIN_PAGE'] = 'examples/login';
---
+++ application/controllers/Home_Controller.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home_Controller extends MY_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->library('twig');
+	}
+
+	public function index()
+	{
+		$this->twig->display('homepage', []);
+	}
+}
