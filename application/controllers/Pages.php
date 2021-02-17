c15693b2ddcc35105ce2ca98e0623de022af4b5f
Oliver Earl <oliver@oliverearl.co.uk>
2021-02-17 18:14:52 +0000

    Slowly adding PHP7 hinting

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,24 +1,24 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pages extends MY_Controller
+class Pages extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
     }
 
-    public function index()
+    public function index(): void
     {
         $this->homepage();
     }
 
-    public function homepage()
+    public function homepage(): void
     {
         $this->twig->display('homepage');
     }
 
-    public function not_found()
+    public function not_found(): void
     {
         // TODO: 404 error-handling
         $this->homepage();
