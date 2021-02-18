63d07c7898cb20103bd605bff07f72194fe06d51
Oliver Earl <oliver@oliverearl.co.uk>
2021-02-18 22:47:45 +0000

    Refactored language files

--- application/language/english/english_lang.php
+++ application/language/english/english_lang.php
@@ -10,25 +10,27 @@ $lang['meta']['description'] = 'The Aberystwyth branch of Nightline, the student
 /**
  * Header / Navigation
  */
-$lang['nav']['home'] = 'Home';
-$lang['nav']['talk'] = 'Talk to Nightline';
-$lang['nav']['call'] = 'Call Us';
-$lang['nav']['chat'] = 'Chat Online';
-$lang['nav']['email'] = 'Email Us';
-$lang['nav']['volunteering'] = 'Volunteering';
-$lang['nav']['faqs'] = 'FAQs';
-$lang['nav']['about'] = 'About Us';
-$lang['nav']['what-is-nightline'] = 'What is Nightline?';
-$lang['nav']['principles'] = 'Our Principles';
-$lang['nav']['confidentiality'] = 'Confidentiality Policy';
-$lang['nav']['testimonials'] = 'Testimonials';
-$lang['nav']['contact'] = 'Contact Us';
-$lang['nav']['social'] = 'Social Media';
-$lang['nav']['facebook'] = 'Like us on Facebook';
-$lang['nav']['twitter'] = 'Follow us on Twitter';
-$lang['nav']['neptune'] = 'Befriend Neptune Nightline';
-$lang['nav']['other-language'] = '🏴󠁧󠁢󠁷󠁬󠁳󠁿 Cymraeg';
-$lang['nav']['other-language-link'] = 'cy';
+$lang['nav'] = [
+    'home' => 'Home',
+    'talk' => 'Talk to Nightline',
+    'call' => 'Call Us',
+    'chat' => 'Chat Online',
+    'email' => 'Email Us',
+    'volunteering' => 'Volunteering',
+    'faqs' => 'FAQs',
+    'about' => 'About Us',
+    'what-is-nightline' => 'What is Nightline?',
+    'principles' => 'Our Principles',
+    'confidentiality' => 'Confidentiality Policy',
+    'testimonials' => 'Testimonials',
+    'contact' => 'Contact Us',
+    'social' => 'Social Media',
+    'facebook' => 'Like us on Facebook',
+    'twitter' => 'Follow us on Twitter',
+    'neptune' => 'Befriend Neptune Nightline',
+    'other-language' => '🏴󠁧󠁢󠁷󠁬󠁳󠁿 Cymraeg',
+    'other-language-link' => 'cy',
+];
 
 /**
  * Footer
@@ -39,35 +41,38 @@ $lang['footer']['login'] = 'Volunteer Login';
 /**
  * Homepage
  */
-$lang['homepage']['title'] = 'Home';
-$lang['homepage']['heading'] = "We're here to listen, not lecture.";
-$lang['homepage']['subheading'] = "We're a student-run listening service. Open 8pm to 8am, Tuesday to Sunday.";
-$lang['homepage']['p1'] = "Aberystwyth Nightline is a confidential and anonymous listening and information service. Students do not have to tell us anything about themselves - not even their name. We're run by trained student volunteers on behalf of all students here at Aberystwyth University.";
-$lang['homepage']['p2'] = "We are non-judgmental and non-directive; seeking to assist callers with any problems or questions that they have. We won't ever push you to talk about something you don't want to.";
-$lang['homepage']['p3'] = "No issue is too big or too small. Whether you're having a bad day, you're in a crisis, or just need to talk. We're here for you.";
-$lang['homepage']['p4'] = 'Follow us on <a href="https://www.facebook.com/abernightline">Facebook</a> and <a href="https://www.twitter.com/abernightline">Twitter</a> for updates relating to our service and opening times!';
-$lang['homepage']['p5'] = "Remember, we're here to listen.";
-$lang['homepage']['twitter-heading'] = 'Tweets by AberNightline';
-$lang['homepage']['need-to-talk'] = 'Need to talk?';
-$lang['homepage']['box-one-title'] = 'Call Nightline';
-$lang['homepage']['box-one-content'] = 'Our longest running service. Whatever the reason, you can pick up the phone and talk to one of our trained volunteers.';
-$lang['homepage']['box-one-button'] = 'Call Us';
-$lang['homepage']['box-two-title'] = 'Instant Messaging';
-$lang['homepage']['box-two-content'] = 'Talk to our volunteers in real time on your computer, phone or tablet using our chat app, in complete anonymity.';
-$lang['homepage']['box-two-button'] = 'Chat Online';
-$lang['homepage']['box-three-title'] = 'Email Listening';
-$lang['homepage']['box-three-content'] = 'No matter what you want to talk about, you can drop us an email and we will talk to you in complete confidence.';
-$lang['homepage']['box-three-button'] = 'Email Us';
-$lang['homepage']['feedback-title'] = 'Have you spoken to us recently? Anything we can do better?';
-$lang['homepage']['feedback-content'] = "We're always looking to improve ourselves and would love to get your anonymous feedback.";
-$lang['homepage']['feedback-button'] = 'Give Feedback';
+$lang['homepage'] = [
+    'title' => 'Home',
+    'heading' => "We're here to listen, not lecture.",
+    'subheading' => "We're a student-run listening service. Open 8pm to 8am, Tuesday to Sunday.",
+    'p1' => "Aberystwyth Nightline is a confidential and anonymous listening and information service. Students do not have to tell us anything about themselves - not even their name. We're run by trained student volunteers on behalf of all students here at Aberystwyth University.",
+    'p2' => "We are non-judgmental and non-directive; seeking to assist callers with any problems or questions that they have. We won't ever push you to talk about something you don't want to.",
+    'p3' => "No issue is too big or too small. Whether you're having a bad day, you're in a crisis, or just need to talk. We're here for you.",
+    'p4' => 'Follow us on <a href="https://www.facebook.com/abernightline">Facebook</a> and <a href="https://www.twitter.com/abernightline">Twitter</a> for updates relating to our service and opening times!',
+    'p5' => "Remember, we're here to listen.",
+    'twitter-heading' => 'Tweets by AberNightline',
+    'box-one-title' => 'Need to talk?',
+    'box-one-content' => 'Our longest running service. Whatever the reason, you can pick p the phone and talk to one of our trained volunteers.',
+    'box-one-button' => 'Call Us',
+    'box-two-title' => 'Instant Messaging',
+    'box-two-content' => 'Talk to our volunteers in real time on your computer, phone or tablet using our chat app, in complete anonymity.',
+    'box-two-button' => 'Chat Online',
+    'box-three-title' => 'Email Listening',
+    'box-three-content' => 'No matter what you want to talk about, you can drop us an email and we will talk to you in complete confidence.',
+    'box-three-button' => 'Email Us',
+    'feedback-title' => 'Have you spoken to us recently? Anything we can do better?',
+    'feedback-content' => "We're always looking to improve ourselves and would love to get your anonymous feedback.",
+    'feedback-button' => 'Give Feedback',
+];
 
 /**
  * Login
  */
-$lang['login']['title'] = 'Login';
-$lang['login']['heading'] = 'Login to Nightlight';
-$lang['login']['login-button'] = 'Login';
-$lang['login']['email'] = 'Email Address';
-$lang['login']['password'] = 'Password';
-$lang['login']['return'] = 'Return to Website';
+$lang['login'] = [
+    'title' => 'Login',
+    'heading' => 'Login to Nightlight',
+    'login-button' => 'Login',
+    'email' => 'Email Address',
+    'password' => 'Password',
+    'return' => 'Return to Website',
+];
