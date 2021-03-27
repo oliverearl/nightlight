bf01ff1270300b850b4eeb1120cd93c08c507637
Oliver Earl <oliver@oliverearl.co.uk>
2021-03-27 17:30:43 +0000

    PHPUnit fix
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- application/config/nightlight.php
+++ application/config/nightlight.php
@@ -4,11 +4,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 /**
  * Custom Nightlight config parameters
  */
-define('CHAT_SYSTEM_ZOHO', 'zoho');
-define('CHAT_SYSTEM_MIBEW', 'mibew');
-define('CHAT_SYSTEMS', [
-    CHAT_SYSTEM_MIBEW,
-    CHAT_SYSTEM_ZOHO,
-]);
+if (!defined('CHAT_SYSTEMS')) {
+    define('CHAT_SYSTEM_ZOHO', 'zoho');
+    define('CHAT_SYSTEM_MIBEW', 'mibew');
+    define('CHAT_SYSTEMS', [
+        CHAT_SYSTEM_MIBEW,
+        CHAT_SYSTEM_ZOHO,
+    ]);
+}
 
 $config['nightlight_chat_system'] = CHAT_SYSTEM_MIBEW;
