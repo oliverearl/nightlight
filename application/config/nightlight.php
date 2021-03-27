f10b73d83e270e8a95411dbf713074adba4603c3
Oliver Earl <oliver@oliverearl.co.uk>
2021-03-27 17:23:11 +0000

    Custom Nightlight configurations
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

---
+++ application/config/nightlight.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/**
+ * Custom Nightlight config parameters
+ */
+define('CHAT_SYSTEM_ZOHO', 'zoho');
+define('CHAT_SYSTEM_MIBEW', 'mibew');
+define('CHAT_SYSTEMS', [
+    CHAT_SYSTEM_MIBEW,
+    CHAT_SYSTEM_ZOHO,
+]);
+
+$config['nightlight_chat_system'] = CHAT_SYSTEM_MIBEW;
