a4eb7221af42d3ae0d7d47241a99dd5776f5f937
Oliver Earl <oliver@oliverearl.co.uk>
2021-04-03 00:37:43 +0100

    Load IM chat settings from env
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- application/config/nightlight.php
+++ application/config/nightlight.php
@@ -5,12 +5,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
  * Custom Nightlight config parameters
  */
 if (!defined('CHAT_SYSTEMS')) {
-    define('CHAT_SYSTEM_ZOHO', 'zoho');
-    define('CHAT_SYSTEM_MIBEW', 'mibew');
+    define('CHAT_SYSTEM_NLA', 'mibew');
+    define('CHAT_SYSTEM_LEGACY', 'zoho');
     define('CHAT_SYSTEMS', [
-        CHAT_SYSTEM_MIBEW,
-        CHAT_SYSTEM_ZOHO,
+        CHAT_SYSTEM_NLA,
+        CHAT_SYSTEM_LEGACY,
     ]);
 }
 
-$config['nightlight_chat_system'] = CHAT_SYSTEM_MIBEW;
+// Interestingly, if the array_search returns false, it's coerced into zero and returns the first index as a default
+$config['nightlight_chat_system'] = CHAT_SYSTEMS[
+    array_search(getenv('NIGHTLIGHT_CHAT_SYSTEM'), CHAT_SYSTEMS)
+];
