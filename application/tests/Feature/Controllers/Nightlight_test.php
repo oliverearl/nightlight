e3502b14abec0ce09f7c3c2a6be5130905d3f2a3
Oliver Earl <oliver@oliverearl.co.uk>
2021-04-03 00:57:36 +0100

    Update tests to make use of helper requests
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- application/tests/Feature/Controllers/Nightlight_test.php
+++ application/tests/Feature/Controllers/Nightlight_test.php
@@ -17,20 +17,28 @@ class Nightlight_test extends TestCase
 
     public function test_nightlight_chat_system_being_set_to_zoho_enables_the_legacy_instant_messaging_system(): void
     {
-        get_instance()->config->set_item('nightlight_chat_system', CHAT_SYSTEM_ZOHO);
+        $this->request('GET', '/testing/chat/legacy');
+
+        $this->assertResponseCode(204);
 
         $output = $this->request('GET', '/chat');
+
+        $this->assertResponseCode(200);
 
         $this->assertContains('http://chat.zoho.com', $output);
     }
 
     public function test_nightlight_chat_system_being_set_to_mibew_enables_the_standard_instant_messaging_system(): void
     {
-        get_instance()->config->set_item('nightlight_chat_system', CHAT_SYSTEM_MIBEW);
+        $this->request('GET', '/testing/chat/nla');
+
+        $this->assertResponseCode(204);
 
         $output = $this->request('GET', '/chat');
 
         $this->assertContains('trialling our new instant messaging system', $output);
+
+        $this->assertResponseCode(200);
 
         $this->assertContains('https://im.aberystwyth.nightline.ac.uk/webim/client.php', $output);
     }
